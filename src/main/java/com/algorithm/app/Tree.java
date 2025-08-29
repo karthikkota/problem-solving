05d9c8ea3ffdbabb0bb984715b53aba3792834bc
Karthik Kota <karthik.kanuparthi@gmail.com>
2025-08-29 10:52:53 -0500

    Update Tree.java

--- src/main/java/com/algorithm/app/Tree.java
+++ src/main/java/com/algorithm/app/Tree.java
@@ -2,6 +2,17 @@ package com.algorithm.app;
 
 public class Tree {
 
+    // LC 94
+    List<Integer> list = new ArrayList();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root != null) {
+            inorderTraversal(root.left);
+            list.add(root.val);
+            inorderTraversal(root.right);
+        }
+        return list;
+    }
+    
     // LC 700
     public TreeNode searchBST(TreeNode root, int val) {
         if (root == null || root.val == val)
