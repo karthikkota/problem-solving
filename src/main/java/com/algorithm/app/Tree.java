d068927c05bb404431c45d7391c73eacea7ba284
Karthik Kota <karthik.kanuparthi@gmail.com>
2025-08-29 11:31:20 -0500

    Add solution for LC 100

--- src/main/java/com/algorithm/app/Tree.java
+++ src/main/java/com/algorithm/app/Tree.java
@@ -2,6 +2,19 @@ package com.algorithm.app;
 
 public class Tree {
 
+    // LC 100
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+            return true;
+
+        if ((p == null && q != null) || (p != null && q == null))
+            return false;
+
+        if (p.val != q.val)
+            return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+    
     // LC 94
     List<Integer> list = new ArrayList();
     public List<Integer> inorderTraversal(TreeNode root) {
