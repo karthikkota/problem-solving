1eb66d76d21effb4756217627d4a97fa91aee8a1
Karthik Kota <karthik.kanuparthi@gmail.com>
2025-09-03 13:04:55 -0500

    Add Solution for LC 404

--- src/main/java/com/algorithm/app/Tree.java
+++ src/main/java/com/algorithm/app/Tree.java
@@ -2,6 +2,21 @@ package com.algorithm.app;
 
 public class Tree {
 
+    // LC 404
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        int sum = 0;
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            sum += root.left.val;
+        }
+
+        sum += sumOfLeftLeaves(root.left);
+        sum += sumOfLeftLeaves(root.right);
+        return sum;
+    }
+    
     // LC 700
     public TreeNode searchBST(TreeNode root, int val) {
         if (root == null || root.val == val) return root;
