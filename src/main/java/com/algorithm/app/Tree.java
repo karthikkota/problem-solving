e3ba190aa3f36b24f72faa24a86618945d305377
Karthik Kota <karthik.kanuparthi@gmail.com>
2025-09-03 13:59:50 -0500

    Add solutions for LC easy tree problems

--- src/main/java/com/algorithm/app/Tree.java
+++ src/main/java/com/algorithm/app/Tree.java
@@ -2,6 +2,18 @@ package com.algorithm.app;
 
 public class Tree {
 
+    // LC 965
+    public boolean isUnivalTree(TreeNode root) {
+        if (root == null)  return false;
+        return isUnivalTree(root, root.val);
+    }
+
+    private boolean isUnivalTree(TreeNode root, int val) {
+        if (root == null)  return true;
+        if (root.val != val)  return false;
+        return isUnivalTree(root.left, val) && isUnivalTree(root.right, val);
+    }
+    
     // LC 589
     public List<Integer> preorder(Node root) {
         List<Integer> list = new ArrayList();
