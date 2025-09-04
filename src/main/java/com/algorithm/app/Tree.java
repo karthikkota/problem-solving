7e21621bac82b5f1a63488f5e7e26b29760937f3
Karthik Kota <karthik.kanuparthi@gmail.com>
2025-09-04 12:47:20 -0500

    Add solution for LC 997

--- src/main/java/com/algorithm/app/Tree.java
+++ src/main/java/com/algorithm/app/Tree.java
@@ -1,6 +1,25 @@
 package com.algorithm.app;
 
 public class Tree {
+    
+    // LC 997
+    public int findJudge(int n, int[][] trust) {
+        if (trust.length < n - 1)
+            return -1;
+
+        int[] trustR = new int[n + 1];
+        for (int[] r : trust) {
+            trustR[r[0]]--;
+            trustR[r[1]]++;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (trustR[i] == n - 1)
+                return i;
+        }
+        return -1;
+    }
+    
     // LC 2689
     StringBuilder res = new StringBuilder();
     public char getKthCharacter(RopeTreeNode root, int k) {
