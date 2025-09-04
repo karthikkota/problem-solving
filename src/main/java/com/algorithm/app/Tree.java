80c8c4c94d1334e47b8e3d5a5a5249667b1c32bb
Karthik Kota <karthik.kanuparthi@gmail.com>
2025-09-04 18:31:55 -0500

    Add solution to LC 101 Iterative

--- src/main/java/com/algorithm/app/Tree.java
+++ src/main/java/com/algorithm/app/Tree.java
@@ -1,6 +1,30 @@
 package com.algorithm.app;
 
 public class Tree {
+
+    // LC 101 Iterative
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList();
+        q.add(root);
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            TreeNode n1 = q.poll();
+            TreeNode n2 = q.poll();
+            if (n1 == null && n2 == null)
+                continue;
+            if (n1 == null || n2 == null)
+                return false;
+            if (n1.val != n2.val)
+                return false;
+            q.add(n1.left);
+            q.add(n2.right);
+            q.add(n1.right);
+            q.add(n2.left);
+        }
+        return true;
+    }
+    
     // LC 94 Iterative
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList();
