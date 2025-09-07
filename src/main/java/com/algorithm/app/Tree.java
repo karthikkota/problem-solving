8c150684ad785e0a1593243752ffb500f09b171d
Karthik Kota <karthik.kanuparthi@gmail.com>
2025-09-07 10:36:39 -0500

    Iterative solutions for binary tree problems II

--- src/main/java/com/algorithm/app/Tree.java
+++ src/main/java/com/algorithm/app/Tree.java
@@ -2,6 +2,30 @@ package com.algorithm.app;
 
 public class Tree {
 
+    // LC 111 Iterative
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        Queue<TreeNode> q = new LinkedList();
+        q.add(root);
+        int depth = 1;
+
+        while (!q.isEmpty()) {
+            int qSize = q.size();
+            while (qSize > 0) {
+                qSize--;
+                TreeNode node = q.remove();
+                if (node == null) continue;
+
+                if (node.left == null && node.right == null) return depth;
+                q.add(node.left);
+                q.add(node.right);
+            }
+            depth++;
+        }
+        return -1;
+    }
+    
     // LC 104 Iterative
     public int maxDepth(TreeNode root) {
         if (root == null)
