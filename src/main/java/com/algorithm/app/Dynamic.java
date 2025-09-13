fc53c2fc8eb0cb0f6aeca7098e113ecf03f05a20
Karthik Kota <karthik.kanuparthi@gmail.com>
2025-09-13 16:08:13 -0500

    Add solution for LC 118

--- src/main/java/com/algorithm/app/Dynamic.java
+++ src/main/java/com/algorithm/app/Dynamic.java
@@ -1,7 +1,20 @@
 package com.algorithm.app;
 
 public class Dynamic {
-
+    
+    // LC 118
+    public int climbStairs(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        int[] mem = new int[n + 1];
+        for (int i = 3; i <= n; i++) {
+            mem[i] = mem[i - 1] + mem[i + 1];
+        }
+        return mem[n];
+    }
+    
+    // LC 70
     public int climbStairs(int n) {
         if (n == 0) return 0;
         if (n == 1) return 1;
