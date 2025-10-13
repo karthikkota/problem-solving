64211b82da4b12af3c7616757bee8e788cd15860
Karthik Kota <karthik.kanuparthi@gmail.com>
2025-10-13 13:33:47 -0500

    Add solution to LC 62

--- src/main/java/com/algorithm/app/Dynamic.java
+++ src/main/java/com/algorithm/app/Dynamic.java
@@ -1,7 +1,21 @@
 package com.algorithm.app;
 
 public class Dynamic {
+    // LC 62
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int[] arr : dp) {
+            Arrays.fill(arr, 1);
+        }
 
+        for (int col = 1; col < m; col++) {
+            for (int row = 1; row < n; row++) {
+                dp[col][row] = dp[col - 1][row] + dp[col][row - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+    
     // LC 53
     public int maxSubArray(int[] nums) {
         int curSum = nums[0], maxSum = nums[0];
