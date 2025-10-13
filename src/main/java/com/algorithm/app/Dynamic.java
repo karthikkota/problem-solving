3a089b76ea4eecb4a60e6b8da436313e062908e9
Karthik Kota <karthik.kanuparthi@gmail.com>
2025-10-13 07:56:41 -0500

    DP set 3

--- src/main/java/com/algorithm/app/Dynamic.java
+++ src/main/java/com/algorithm/app/Dynamic.java
@@ -2,6 +2,27 @@ package com.algorithm.app;
 
 public class Dynamic {
 
+    // LC 5
+    public String longestPalindrome(String s) {
+        int end = 0, start = 0, maxLen = 1, n = s.length();
+        boolean[][] dp = new boolean[n][n];
+
+        for (int i = 0; i < s.length(); ++i) {
+            dp[i][i] = true;
+            for (int j = 0; j < i; ++j) {
+                if (s.charAt(j) == s.charAt(i) && (i - j <= 2 || dp[j + 1][i - 1])) {
+                    dp[j][i] = true;
+                    if (i - j + 1 > maxLen) {
+                        maxLen = i - j + 1;
+                        start = j;
+                        end = i;
+                    }
+                }
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+    
     // LC 392 DP
     public boolean isSubsequence(String s, String t) {
         int sLen = s.length(), tLen = t.length();
