3a71179b591060b9aefa8c935ac09a0f568d9718
Karthik Kota <karthik.kanuparthi@gmail.com>
2025-11-23 05:57:03 -0600

    Add LC 739 stack problem

---
+++ src/main/java/com/algorithm/app/Stack.java
@@ -0,0 +1,21 @@
+package com.algorithm.app;
+
+public class Stack {
+
+  // LC 739
+  public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] answer = new int[n];
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int currDay = 0; currDay < n; currDay++) {
+            int currentTemp = temperatures[currDay];
+            while (!stack.isEmpty() && temperatures[stack.peek()] < currentTemp) {
+                int prevDay = stack.pop();
+                answer[prevDay] = currDay - prevDay;
+            }
+            stack.push(currDay);
+        }
+        return answer;
+    }
+}
