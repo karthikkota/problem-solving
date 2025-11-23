14608428fe67446a1496dcd6a48b9b1714c41198
Karthik Kota <karthik.kanuparthi@gmail.com>
2025-11-23 00:10:00 -0600

    Add solution to LC 167

--- src/main/java/com/algorithm/app/TwoPointers.java
+++ src/main/java/com/algorithm/app/TwoPointers.java
@@ -1,6 +1,23 @@
 package com.algorithm.app;
 
 public class TwoPointers {
+  // LC 167
+  public int[] twoSum(int[] numbers, int target) {
+        int low = 0;
+        int high = numbers.length - 1;
+
+        while (low < high) {
+            int curSum = numbers[low] + numbers[high];
+            if (curSum > target) {
+                high--;
+            } else if (curSum < target) {
+                low++;
+            } else {
+                return new int[] { low + 1, high + 1 };
+            }
+        }
+        return new int[] { -1, -1 };
+    }
   
   // LC 922
   public int[] sortArrayByParityII(int[] nums) {
