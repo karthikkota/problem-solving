4f37964d00a2ab3eea2c4d8119ac46efd31cc292
Karthik Kota <karthik.kanuparthi@gmail.com>
2025-12-01 06:24:17 -0600

    Add LC 704 solution

---
+++ src/main/java/com/algorithm/app/BinarySearch.java
@@ -0,0 +1,20 @@
+package com.algorithm.app;
+
+public class BinarySearch {
+
+  // LC 704
+  public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
