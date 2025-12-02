f979d3b702a6546345506c46a707c6616e7b4692
Karthik Kota <karthik.kanuparthi@gmail.com>
2025-12-02 10:14:15 -0600

    Add solution to LC 74

--- src/main/java/com/algorithm/app/BinarySearch.java
+++ src/main/java/com/algorithm/app/BinarySearch.java
@@ -2,6 +2,25 @@ package com.algorithm.app;
 
 public class BinarySearch {
 
+  // LC 74
+  public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int l = 0, r = m * n - 1;
+        int pivotIdx, pivotElement;
+        while (l <= r) {
+            pivotIdx = (l + r) / 2;
+            pivotElement = matrix[pivotIdx / n][pivotIdx % n];
+            if (pivotElement < target) {
+                l = pivotIdx + 1;
+            } else if (pivotElement > target) {
+                r = pivotIdx - 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+  
   // LC 704
   public int search(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
