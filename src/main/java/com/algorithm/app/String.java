87f0a0bb5bc02bb8abbd75b02d4116fad822b0c7
Karthik Kota <karthik.kanuparthi@gmail.com>
2026-01-17 03:34:42 -0800

    Add solution to LC 205
    Add method to check if two strings are isomorphic.

--- src/main/java/com/algorithm/app/String.java
+++ src/main/java/com/algorithm/app/String.java
@@ -2,6 +2,23 @@ package com.algorithm.app;
 
 public class String {
 
+    // LC 205
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character, Character> stMap = new HashMap<Character, Character>();        
+        for (int i = 0; i < s.length(); i++) {
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
+            if (stMap.containsKey(sChar) && stMap.get(sChar) != tChar) {
+                return false;
+            } else if (!stMap.containsKey(sChar) && stMap.containsValue(tChar)) {
+                return false;
+            } else if (!stMap.containsKey(sChar) && !stMap.containsValue(tChar)) {
+                stMap.put(sChar, tChar);
+            }
+        }
+        return true;
+    }
+    
     // LC 168
     public String convertToTitle(int columnNumber) {
         StringBuilder colTitle = new StringBuilder();
