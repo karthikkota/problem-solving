06c54b4e5be3877ab6bb6a869083cd06bc618022
Karthik Kota <karthik.kanuparthi@gmail.com>
2026-01-21 02:55:35 -0800

    Add solution to LC 219

--- src/main/java/com/algorithm/app/Array.java
+++ src/main/java/com/algorithm/app/Array.java
@@ -2,6 +2,19 @@ package com.algorithm.app;
 
 public class Array {
 
+    // LC 219
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> numSet = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (numSet.contains(nums[i]))
+                return true;
+            numSet.add(nums[i]);
+            if (numSet.size() > k)
+                numSet.remove(nums[i - k]);
+        }
+        return false;
+    }
+    
     // LC 169
     public int majorityElement(int[] nums) {
         int count = 0, candidate = 0;
