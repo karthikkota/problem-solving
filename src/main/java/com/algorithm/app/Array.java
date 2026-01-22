63cfb9e9220804b94a3bb5a8fb06526c8bf5ccb0
Karthik Kota <karthik.kanuparthi@gmail.com>
2026-01-22 08:56:12 -0800

    Add solution to LC 252

--- src/main/java/com/algorithm/app/Array.java
+++ src/main/java/com/algorithm/app/Array.java
@@ -2,6 +2,16 @@ package com.algorithm.app;
 
 public class Array {
 
+    // LC 252
+    public boolean canAttendMeetings(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        for (int i = 0; i < intervals.length - 1; i++) {
+            if (intervals[i][1] > intervals[i + 1][0])
+                return false;
+        }
+        return true;
+    }
+    
     // LC 219
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         Set<Integer> numSet = new HashSet<>();
