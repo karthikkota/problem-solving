e1eeac2738f2c07221cd09807f414ff15f2b5d18
Karthik Kota <karthik.kanuparthi@gmail.com>
2026-04-09 13:15:32 -0500

    Refactor findMin method in BinarySearch
    Refactor findMin method to simplify logic and improve readability.

--- src/main/java/com/algorithm/app/BinarySearch.java
+++ src/main/java/com/algorithm/app/BinarySearch.java
@@ -25,23 +25,17 @@ public class BinarySearch {
   }
   
   // LC 153
-  public int findMin(int[] nums) {
-        if (nums.length == 1) return nums[0];
+    public int findMin(int[] nums) {
         int left = 0, right = nums.length - 1;
-        if (nums[left] < nums[right]) return nums[left];
-        
-        while (right >= left) {
+        while (left < right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] > nums[mid + 1]) return nums[mid + 1];
-            if (nums[mid - 1] > nums[mid]) return nums[mid];
-
-            if (nums[mid] > nums[0]) {
-                left = mid + 1;
+            if (nums[mid] <= nums[right]) {
+                right = mid;
             } else {
-                right = mid - 1;
+                left = mid + 1;
             }
         }
-        return Integer.MIN_VALUE;
+        return nums[left];
     }
   
   // LC 74
