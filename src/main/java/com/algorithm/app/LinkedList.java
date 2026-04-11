3c2f0b5269e07a17c52d0276a4f0b39926ad9446
Karthik Kota <karthik.kanuparthi@gmail.com>
2026-04-11 14:13:47 -0500

    Add mergeTwoLists method to LinkedList class

--- src/main/java/com/algorithm/app/LinkedList.java
+++ src/main/java/com/algorithm/app/LinkedList.java
@@ -2,6 +2,24 @@ package com.algorithm.app;
 
 public class LinkedList {
 
+  // LC 21
+  public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode preHead = new ListNode(-1), cur = preHead;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                cur.next = list1;
+                list1 = list1.next;
+            } else {
+                cur.next = list2;
+                list2 = list2.next;
+            }
+            cur = cur.next;
+        }
+
+        cur.next = list1 == null ? list2 : list1;
+        return preHead.next;
+    }
+  
   // LC 206
   public ListNode reverseList(ListNode head) {
         ListNode prev = null, cur = head;
