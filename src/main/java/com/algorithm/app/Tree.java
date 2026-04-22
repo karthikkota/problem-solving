5eb566f05edb750ba3c361e409a1bb44431be88c
Karthik Kota <karthik.kanuparthi@gmail.com>
2026-04-22 14:23:12 -0500

    Implement tree construction from preorder and inorder

--- src/main/java/com/algorithm/app/Tree.java
+++ src/main/java/com/algorithm/app/Tree.java
@@ -1,6 +1,28 @@
 package com.algorithm.app;
 
 public class Tree {
+
+    // LC 105
+    int preorderIdx = 0;
+    Map<Integer, Integer> inorderMap = new HashMap<>();
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        for (int i = 0; i < inorder.length; i++) {
+            inorderMap.put(inorder[i], i);
+        }
+        return dfs(preorder, 0, inorder.length - 1);
+    }
+
+    private TreeNode dfs(int[] preorder, int left, int right) {
+        if (left > right)
+            return null;
+        int rootVal = preorder[preorderIdx++];
+        TreeNode root = new TreeNode(rootVal);
+        int mid = inorderMap.get(rootVal);
+        root.left = dfs(preorder, left, mid - 1);
+        root.right = dfs(preorder, mid + 1, right);
+        return root;
+    }
     
     // 230
     List<Integer> bstInorderArr = new ArrayList<>();
