feabce3e9e1a6a2db581910ceb3e60a47c6c1500
Karthik Kota <karthik.kanuparthi@gmail.com>
2026-04-23 16:16:11 -0500

    Add leastInterval method to Heap class
    Implemented leastInterval method to calculate minimum CPU intervals for tasks with cooling time.

--- src/main/java/com/algorithm/app/Heap.java
+++ src/main/java/com/algorithm/app/Heap.java
@@ -1,6 +1,23 @@
 package com.algorithm.app;
 
 public class Heap {
+
+  // LC 621
+  public int leastInterval(char[] tasks, int n) {
+        int[] freq = new int[26];
+        int maxFreq = 0;
+
+        for (char t : tasks) {
+            freq[t - 'A']++;
+            maxFreq = Math.max(maxFreq, freq[t - 'A']);
+        }
+
+        int cpuTime = (maxFreq - 1) * (n + 1);
+        for (int f : freq) {
+            if (f == maxFreq) cpuTime++;
+        }
+        return Math.max(cpuTime, tasks.length);
+    }
   
   // LC 1046
   public int lastStoneWeight(int[] stones) {
