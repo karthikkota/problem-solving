090a86c0b3495dfc33f3f0b171c54f30c0e7ce44
Karthik Kota <karthik.kanuparthi@gmail.com>
2026-04-24 17:57:12 -0500

    Add backtracking methods for combination sum and permutations

---
+++ src/main/java/com/algorithm/app/Backtracking.java
@@ -0,0 +1,54 @@
+package com.algorithm.app;
+
+public class Backtracking {
+    // LC 40
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> finalList = new ArrayList<>();
+        Arrays.sort(candidates);
+        backtrack(candidates, new ArrayList<>(), finalList, target, 0);
+        return finalList;
+    }
+
+  private void backtrack(int[] candidates, List<Integer> curList, List<List<Integer>> finalList, int target,
+            int startIdx) {
+        if (target == 0) {
+            finalList.add(new ArrayList<Integer>(curList));
+            return;
+        }
+
+        if (target < 0)
+            return;
+
+        for (int i = startIdx; i < candidates.length; i++) {
+            if (candidates[i] > target)
+                break;
+            if (i > startIdx && candidates[i] == candidates[i - 1])
+                continue;
+            curList.add(candidates[i]);
+            backtrack(candidates, curList, finalList, target - candidates[i], i + 1);
+            curList.remove(curList.size() - 1);
+        }
+    }
+
+  // LC 46
+  public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> permutations = new ArrayList<>();
+        backtrack(nums, new ArrayList<>(), permutations);
+        return permutations;
+    }
+
+    private void backtrack(int[] nums, List<Integer> curList, List<List<Integer>> permutations) {
+        if (curList.size() == nums.length) {
+            permutations.add(new ArrayList<>(curList));
+            return;
+        }
+
+        for (int n : nums) {
+            if (curList.contains(n))
+                continue;
+            curList.add(n);
+            backtrack(nums, curList, permutations);
+            curList.remove(curList.size() - 1);
+        }
+    }
+}
